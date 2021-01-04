fa01a1b530212bb13d2b8404dda78c8b8fe95728
rupesh1010 <76780243+rupesh1010@users.noreply.github.com>
2021-01-04 19:06:29 +0530

    Update factorial.py

--- Python Assingments/factorial.py
+++ Python Assingments/factorial.py
@@ -2,7 +2,8 @@ def factorial(n):
 	if n==0 or n==1:
 		return 1
 	else:
-		return n*factorial(n-1)
+		#Using recursion
+		return n*factorial(n-1)		
 		
 n=int(input("Enter any number: "))
 fact=factorial(n)
